ae6db202bc0f9a3a725ad195722f7dda0c3eae7e
MalenFrontedDev <sengsomalen.nu@gmail.com>
2026-08-24 13:30:49 +0700

    test dashboard controller

--- Modules/Dashboard/App/Http/Controllers/DashboardController.php
+++ Modules/Dashboard/App/Http/Controllers/DashboardController.php
@@ -13,31 +13,9 @@ class DashboardController extends Controller
     ) {}
 
     public function index()
-    {
-        $data = [
-            'project_statistics' => $this->dashboardService->getProjectStatistics(),
-
-            'task_statistics' => $this->dashboardService->getTaskStatistics(),
-
-            'completed_vs_pending' => $this->dashboardService
-                ->getCompletedVsPendingTasks(),
-
-            'overdue_tasks' => $this->dashboardService
-                ->getOverdueTasks(),
-
-            'recent_projects' => $this->dashboardService
-                ->getRecentProjects(),
-
-            'upcoming_tasks' => $this->dashboardService
-                ->getUpcomingTasks(),
-
-            'recent_activity' => $this->dashboardService
-                ->getRecentActivity(),
-
-            'total_user' => $this->dashboardService
-                ->getTotalUsers(),
-        ];
-
-        return new DashboardResource($data);
-    }
+{
+    return response()->json([
+        'message' => 'Dashboard works'
+    ]);
+}
 }
