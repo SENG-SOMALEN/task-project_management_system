c1d0ab8c12d7e7912af18a6f86c70b7b606e82dd
MalenFrontedDev <sengsomalen.nu@gmail.com>
2026-09-14 20:02:52 +0700

    fix: update CORS allowed origins for Vercel

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://pms-rose-tau.vercel.app',
+        'http://localhost:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
