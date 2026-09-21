b1c2043908ff6601a49ded9bd46a9cc84c7cfe15
MalenFrontedDev <sengsomalen.nu@gmail.com>
2026-09-21 22:12:33 +0700

    Configure database migration and seeding

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserDatabaseSeeder extends Seeder
 {
@@ -12,5 +14,20 @@ class UserDatabaseSeeder extends Seeder
     public function run(): void
     {
         // $this->call([]);
+
+        $users = [
+            [
+                'username'   => 'Malen',
+                'gender'     => 'Male',
+                'email'      => 'malen@gmail.com',
+                'password'   => Hash::make('123456'),
+                'role'       => 'Admin',
+                'status'     => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ];
+
+        DB::table('users')->insert($users);
     }
 }
